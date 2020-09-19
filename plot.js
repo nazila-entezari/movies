731150364f14d203c3ca0fe9f88537303723c284
nazila-entezari <65842917+nazila-entezari@users.noreply.github.com>
2020-09-19 13:28:56 -0400

    Update plot.js

--- plot.js
+++ plot.js
@@ -2,7 +2,7 @@
 // Incoming data is internally referred to as incomingData
 d3.json("data/data.json").then((incomingData) => {
   function filterMovieRatings(movie) {
-    return movie.imdbRating > 3;
+    return movie.imdbRating > 8.9;
   }
 
   // Use filter() to pass the function as its argument
